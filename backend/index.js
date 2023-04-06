53a33fe3ff0355c8b43f618a2dafc8b82d47e881
KartikDang <kartikdang29@gmail.com>
2023-04-06 17:12:59 +0530

    cleaned structure

--- backend/index.js
+++ backend/index.js
@@ -7,13 +7,20 @@ const db = mysql.createConnection({
     host: "localhost",
     user:"root",
     password:"admin",
-    database:"test",
+    database:"new",
 });
 
 
 app.get("/",(req,res)=>{
-
-    res.json("Hello World");
+    // res.json("Hello World");
+    const q = "SELECT * FROM dept2";
+    db.query(q,(err,result)=>{
+        if(err){
+            res.json(err);
+        }else{
+            res.json(result);
+        }
+    })
 })
 
 
