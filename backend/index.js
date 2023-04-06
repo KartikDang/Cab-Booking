dc8999a89f0ca51ed27e8d2dd0620952d86a7a07
KartikDang <kartikdang29@gmail.com>
2023-04-07 01:39:57 +0530

    login api part done

--- backend/index.js
+++ backend/index.js
@@ -23,6 +23,36 @@ app.get("/",(req,res)=>{
     })
 })
 
+app.post("/login",(req,res)=>{
+
+    const email = req.body.email;
+    const password = req.body.password;
+
+
+    const q = "SELECT * from users where email = ?";
+
+    db.query(q,[email],(err,result)=>{
+        if(err){
+            console.log(err);
+            res.status(500).send('Error in database');   
+        }else if(result.length === 0){
+            res.status(400).send('Email not found');
+        }else{
+            var flag = 0;
+            results.map((e)=>{
+                if(e.password===password){
+                    res.send("Login Successful");
+                    flag = 1;
+                }
+            })
+
+            if(flag===0){
+                res.send("Wrong Password");
+            }
+        }
+    })
+})
+
 
 
 
