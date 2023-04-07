febd0c12a13234774ed7276b5963306f21f5fc60
KartikDang <kartikdang29@gmail.com>
2023-04-07 14:22:22 +0530

    Login Form UI Created

--- client/src/pages/login.jsx
+++ client/src/pages/login.jsx
@@ -1,8 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useState } from 'react';
 import "./login.css";
 
 const Login = () => {
+
+    const [email, setEmail] = React.useState("");
+    const [password, setPassword] = React.useState("");
+
+    function handleSubmit(){
+
+    }
+
     return (
         <div>
             {/* NAVBAR */}
@@ -10,12 +19,46 @@ const Login = () => {
                 <div class="container-fluid">
                     <div>
                         {/* <img src="/docs/5.3/assets/brand/bootstrap-logo.svg" alt="Logo" width="30" height="24" class="d-inline-block align-text-top"> */}
-                        <Link to="#" className=" d-flex align-items-center gap-1 navbar-brand nav-login">
+                        <Link to="/" className=" d-flex align-items-center gap-1 navbar-brand nav-login">
                             <h3>Cab Service</h3>
                         </Link>
                     </div>
                 </div>
             </nav>
+
+            <div className="Auth-form-container">
+                <form className="Auth-form">
+                    <div className="Auth-form-content">
+                        <h3 className="Auth-form-title">Sign In</h3>
+                        <div className="form-group mt-3">
+                            <label>Email address</label>
+                            <input
+                                type="email"
+                                className="form-control mt-1"
+                                placeholder="Enter email"
+                                onChange={(e)=>{setEmail(e.target.value)}}
+                            />
+                        </div>
+                        <div className="form-group mt-3">
+                            <label>Password</label>
+                            <input
+                                type="password"
+                                className="form-control mt-1"
+                                placeholder="Enter password"
+                                onChange={(e)=>{setPassword(e.target.value)}}
+                            />
+                        </div>
+                        <div className="d-grid gap-2 mt-3">
+                            <button type="submit" className="btn btn-primary submitbtn" onClick={handleSubmit}>
+                                Submit
+                            </button>
+                        </div>
+                        <p className="forgot-password text-right mt-2">
+                            Forgot password?
+                        </p>
+                    </div>
+                </form>
+            </div>
         </div>
     );
 }
