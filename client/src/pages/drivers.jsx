7c088b84e3044f1748416bc1167cca2dfb7431a4
KartikDang <kartikdang29@gmail.com>
2023-04-10 00:16:48 +0530

    Customer Page x1

--- client/src/pages/drivers.jsx
+++ client/src/pages/drivers.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
 const Drivers = () => {
     return (
         <div>
-            
+             {/* NAVBAR */}
+             <nav class="navbar bg-body-tertiary">
+                <div class="container-fluid">
+                    <div>
+                        {/* <img src="/docs/5.3/assets/brand/bootstrap-logo.svg" alt="Logo" width="30" height="24" class="d-inline-block align-text-top"> */}
+                        <Link to="/" className=" d-flex align-items-center gap-1 navbar-brand nav-login">
+                            <h3>Cab Service</h3>
+                        </Link>
+                    </div>
+                </div>
+
+            </nav>
         </div>
     );
 }
