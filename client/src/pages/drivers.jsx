8ae074015e9b9165bac88bf38354f1d55cef9827
KartikDang <kartikdang29@gmail.com>
2023-04-10 02:12:13 +0530

    Driver Page Active

--- client/src/pages/drivers.jsx
+++ client/src/pages/drivers.jsx
@@ -1,10 +1,52 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { LightningLoader } from '../components/loader';
 export const Drivers = () => {
+
+
+
+    const [isLoaded, setIsLoaded] = React.useState(false);
+    const [res, setRes] = React.useState([]);
+
+
+    async function handleLoad(e) {
+
+        await fetch("http://localhost:8080/alldrivers", {
+            mode: 'cors',
+            method: 'GET',
+            headers: {
+                // 'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            // body:JSON.stringify({email,password})
+        }).then(res => {
+            res.json().then(data => {
+                console.log(data);
+                setIsLoaded(true);
+                setRes(data);
+                // setMake(data[0].model);
+                // setType(data[0].type);
+                // setStatus(data[0].status);
+            })
+        })
+    }
+
+
+    if (!isLoaded) {
+        handleLoad();
+        return (
+            <div className="loader">
+                <LightningLoader />
+            </div>
+        )
+    }
+
+
+
     return (
         <div>
-             {/* NAVBAR */}
-             <nav class="navbar bg-body-tertiary">
+            {/* NAVBAR */}
+            <nav class="navbar bg-body-tertiary">
                 <div class="container-fluid">
                     <div>
                         {/* <img src="/docs/5.3/assets/brand/bootstrap-logo.svg" alt="Logo" width="30" height="24" class="d-inline-block align-text-top"> */}
@@ -15,6 +57,67 @@ export const Drivers = () => {
                 </div>
 
             </nav>
+
+
+            <div className="table-div">
+                <h3 className="Auth-form-title" style={{ marinTop: "3%" }}>Customers</h3>
+                <table class="table">
+                    <thead>
+                        <tr>
+                            {/* <th scope="col">#</th> */}
+                            <th scope="col">Name</th>
+                            <th scope="col">Email</th>
+                            <th scope="col">Contact</th>
+                            <th scope="col">License Number</th>
+                            <th scope="col">Model</th>
+                            <th scope="col">Type</th>
+                            <th scope="col"></th>
+                        </tr>
+                    </thead>
+                    <tbody class="table-group-divider">
+                        {/* <tr>
+                            <th scope="row">1</th>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td>
+                                {/* <button type="button" class="btn btn-success">Change Status</button> */}
+                        {/* </td> */}
+                        {/* </tr> */}
+
+                        {res.map((e) => {
+                            return (
+                                <tr>
+                                    {/* <th scope = "row"> */}
+                                    <td>
+                                        {e.name}
+                                    </td>
+                                    <td>
+                                        {e.email}
+                                    </td>
+                                    <td>
+                                        {e.contact}
+                                    </td>
+                                    <td>
+                                        {e.license_no}
+                                    </td>
+                                    <td>
+                                        {e.model}
+                                    </td>
+                                    <td>
+                                        {e.type}
+                                    </td>
+
+                                </tr>
+                            )
+                        })
+                        }
+
+
+                    </tbody>
+                </table>
+            </div>
+
         </div>
     );
 }
